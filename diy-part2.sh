51e7913edaf0a8439c0d2bfac4f1200fc60a504d
ywgf88 <43352863+ywgf88@users.noreply.github.com>
2020-09-28 16:23:09 +0800

    OK

--- diy-part2.sh
+++ diy-part2.sh
@@ -32,9 +32,9 @@ sed -i 's/OpenWrt/William build $(date "+%Y.%m.%d") @ OpenWrt/g' package/lean/de
 
 # 删除默认argon主题，并下载新argon主题
 rm -rf ./package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
 #git lua-maxminddb 依赖
-git clone https://github.com/jerrykuku/lua-maxminddb.git  package/lean/lua-maxminddb
+#git clone https://github.com/jerrykuku/lua-maxminddb.git  package/lean/lua-maxminddb
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.8/g' package/base-files/files/bin/config_generate
 # Modify default wireless name
