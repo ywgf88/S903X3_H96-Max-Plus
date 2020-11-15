bc20ee7e2c3ab6a6d10c141f9f4c03df31555f98
ywgf88 <43352863+ywgf88@users.noreply.github.com>
2020-11-15 14:14:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -44,5 +44,5 @@ sed -i 's/OpenWrt/S903X3_H96-Max-Plus/g' package/kernel/mac80211/files/lib/wifi/
 sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
 
 #replace coremark.sh with the new one
-rm package/lean/coremark/coremark.sh
-cp $GITHUB_WORKSPACE/general/coremark.sh package/lean/coremark/
+#rm package/lean/coremark/coremark.sh
+#cp $GITHUB_WORKSPACE/general/coremark.sh package/lean/coremark/
