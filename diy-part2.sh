c40b3f0e8a53dfbf2274971eba7bee4806811009
ywgf88 <43352863+ywgf88@users.noreply.github.com>
2021-03-20 16:44:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -56,8 +56,8 @@ git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dock
 
 #add bypass
 git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
-git clone https://github.com/garypang13/luci-app-dnsfilter package/luci-app-dnsfilter
-git clone https://github.com/project-lede/luci-app-godproxy package/luci-app-godproxy
+#git clone https://github.com/garypang13/luci-app-dnsfilter package/luci-app-dnsfilter
+#git clone https://github.com/project-lede/luci-app-godproxy package/luci-app-godproxy
 git clone https://github.com/garypang13/smartdns-le package/smartdns-le
 
 #svn co https://github.com/garypang13/openwrt-packages/tree/master/luci-app-smartdns
